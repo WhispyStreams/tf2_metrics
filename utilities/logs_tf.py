317f184f13a597d307a23f5c943e7ba8d145132b
Johnny Figueroa <redditjohnny@gmail.com>
2020-12-12 00:47:44 -0800

    started building dataclasses representing a Log

--- utilities/logs_tf.py
+++ utilities/logs_tf.py
@@ -1,12 +1,42 @@
+from dataclasses import dataclass
+
+from types import SimpleNamespace
+from typing import List
+
 import requests
 
 
 def get_match(matchid):
     if match_cached(matchid):
         return match_cache(matchid)
-    return requests.get(f"http://logs.tf/json/{matchid}").json()
+    return requests.get(f"http://logs.tf/json/{matchid}").json(
+        object_hook=lambda d: [] if "players" in d else SimpleNamespace(**d)
+    )
+
+
+@dataclass
+class Team:
+    color: str
+    score: int
+    kills: int
+    deaths: int
+    dmg: int
+    charges: int
+    drops: int
+    firstcaps: int
+    caps: int
+
+
+@dataclass
+class Log:
+    version: str
+    teams: List[Team]
 
 
 # TODO: Determine if a cache/storage mechanism is necessary/useful.
 def match_cached(matchid):
     return False
+
+
+def match_cache(matchid):
+    raise NotImplementedError("Match cache not implemented.")
