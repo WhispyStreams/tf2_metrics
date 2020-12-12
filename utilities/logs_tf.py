92efcf4affefe312c02f6db2127391a1a13750c1
Johnny Figueroa <redditjohnny@gmail.com>
2020-12-12 01:59:29 -0800

    Added download_log_zip

--- utilities/logs_tf.py
+++ utilities/logs_tf.py
@@ -9,9 +9,7 @@ import requests
 def get_match(matchid):
     if match_cached(matchid):
         return match_cache(matchid)
-    return requests.get(f"http://logs.tf/json/{matchid}").json(
-        object_hook=lambda d: [] if "players" in d else SimpleNamespace(**d)
-    )
+    return requests.get(f"http://logs.tf/json/{matchid}").json()
 
 
 @dataclass
@@ -29,8 +27,25 @@ class Team:
 
 @dataclass
 class Log:
+    matchid: int
     version: str
     teams: List[Team]
+
+
+def download_log_zip(matchid):
+    from zipfile import ZipFile
+    from io import BytesIO
+
+    zip_request = requests.get(f"https://logs.tf/logs/log_{matchid}.log.zip")
+
+    # with open(filepath, "wb") as f:
+    #     f.write(zip_request.content)
+
+    # matchzip = ZipFile(filepath)
+
+    match_zip = ZipFile(BytesIO(zip_request.content))
+
+    return match_zip
 
 
 # TODO: Determine if a cache/storage mechanism is necessary/useful.
